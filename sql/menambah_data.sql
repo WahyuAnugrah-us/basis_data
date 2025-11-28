ec37b210a455d878180a9a9fa49c442c22489270
WahyuAnugrah-us <wayyuwahyuanugrahmaiwa@gmail.com>
2025-11-29 01:43:13 +0800

    Create menambah_data.sql

---
+++ sql/menambah_data.sql
@@ -0,0 +1,7 @@
+insert into ruangan values
+    ('R026', 'Ruang Anak C', 'Rawat Inap', 2, 300000),
+    ('R027', 'Ruang Jantung C', 'Rawat Inap', 3, 400000),
+    ('R028', 'Ruang IGD', 'Gawat Darurat', 4, 500000),
+    ('R029', 'Ruang ICU', 'Intensif', 4, 800000),
+    ('R030', 'Ruang Bayi', 'Rawat Inap', 6, 300000);
+
